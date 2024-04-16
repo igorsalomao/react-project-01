9aa5913f30ecfda7fcbc67f19661811f0ad6566a
Igor Salomão <igorsalomao13@gmail.com>
2024-04-16 17:13:24 -0300

    create Results component and make a lifting state up with userInput

---
+++ src/components/Results.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Results({ input }) {
+  
+  console.log(input)
+  return (
+    <p>results...</p>
+    // <table id='result'>
+    //   <thead>
+    //   <tr>
+    //     <th>Year</th>
+    //     <th>Investment Value</th>
+    //     <th>Interest(Year)</th>
+    //     <th>Total Interest</th>
+    //     <th>Invested Capital</th>
+    //   </tr>
+    //   </thead>
+    //   <tbody>
+    //     <tr>
+    //       <td></td>
+    //       <td>{initialInvestment}</td>
+    //       <td></td>
+    //       <td></td>
+    //       <td></td>
+    //     </tr>
+    //   </tbody>
+    // </table>
+  )
+}
